f7ca22b8cdea48479c5beb4f7f2dcf392aa0d16a
jsmnblyss <jaja.srsb@gmail.com>
2026-09-23 23:08:58 +0800

    Add 50% VIP surcharge for premium seating - sarsaba.jasmineblyss

--- tickets.js
+++ tickets.js
@@ -2,8 +2,14 @@ function isValidQuantity(quantity) {
   return quantity > 0 && quantity <= 20;
 }
 
-function calculateTicketPrice(quantity, basePrice) {
-  return Math.floor(quantity * basePrice);
+function calculateTicketPrice(quantity, basePrice, isPremium) {
+  let price = quantity * basePrice;
+
+  if (isPremium) {
+    price = price * 1.50; 
+  }
+
+  return Math.floor(price);
 }
 
 module.exports = { isValidQuantity, calculateTicketPrice };
